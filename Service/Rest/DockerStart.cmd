5a6906157a25a07ece61dd4868dffef214a73296
Dmitry Kiselev <kd.000.000.1@gmail.com>
2017-11-16 01:40:22 +0300

    *

--- Service/Rest/DockerStart.cmd
+++ Service/Rest/DockerStart.cmd
@@ -1,4 +1,4 @@
-docker build -t kd.000.000.1/knight-frank-node-rest-api .
-docker run -p 3000:3000 -d kd.000.000.1/knight-frank-node-rest-api
+docker build -t knight-frank-node-rest-api .
+docker run -p 3000:3000 -d knight-frank-node-rest-api
 
 pause
